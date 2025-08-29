1848f6010dd993dee8037ad7abbd68c739a3a671
AdamskiMax <adamski@devlabor.com>
2025-08-29 10:37:39 +0200

    password tester

--- password_checker.py
+++ password_checker.py
@@ -1,3 +1,5 @@
+import string
+
 # password_checker.py
 def is_valid_password(password):
     """
@@ -11,5 +13,9 @@ def is_valid_password(password):
     if not any(c.isdigit() for c in password):
         return False
     if not any(c.islower() for c in password):
+        return False
+    if not any(c.isupper() for c in password):
+        return False
+    if not any(c in string.punctuation for c in password):
         return False
     return True
